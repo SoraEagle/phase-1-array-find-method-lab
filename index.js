50b44d171863b847ea33205780f0f685b7ac8433
Matthew <phillips.matthew829@gmail.com>
2021-11-09 13:48:15 -0600

    commit tree d76035aa1a1ace4520f88e3e76e48235cff47c94

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+  function superbowlWin(records){
+    let foundRecord = records.find(record => (record.result === 'W'))
+    console.log(foundRecord);
+    if(foundRecord)
+    return foundRecord.year;
+}
